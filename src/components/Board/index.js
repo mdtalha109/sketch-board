a3c3ec8d4cff99898f77e002cd99cf961cf19c6f
mdtalha109 <mdtalha1041@gmail.com>
2023-11-04 02:40:40 +0530

    feat: implemented undo & redo features

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,12 +1,43 @@
+import { actionItemClick } from '@/Redux/slice/menuSlice';
+import { MENU_ITEMS } from '@/constants';
 import React, { useEffect, useLayoutEffect, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 
 const Board = () => {
+  const dispatch = useDispatch()
   const canvasRef = useRef(null)
   const shouldDraw = useRef(false)
+  const drawHistory = useRef([])
+  const historyPointer = useRef(0)
 
-  const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
+  const { activeMenuItem, actionMenuItem } = useSelector((state) => state.menu)
   const { color, size } = useSelector((state) => state.toolbox[activeMenuItem])
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d')
+
+    if (actionMenuItem === MENU_ITEMS.DOWNLOAD) {
+      const URL = canvas.toDataURL()
+      const anchor = document.createElement('a')
+      anchor.href = URL
+      anchor.download = 'sketch.jpg'
+      anchor.click()
+    } 
+    else if (actionMenuItem === MENU_ITEMS.UNDO) {
+      if (historyPointer.current > 0) historyPointer.current -= 1
+      const imageData = drawHistory.current[historyPointer.current]
+      context.putImageData(imageData, 0, 0)
+      dispatch(actionItemClick(null))
+    } 
+    else if (actionMenuItem === MENU_ITEMS.REDO) {
+      if (historyPointer.current < drawHistory.current.length - 1 ) historyPointer.current += 1
+      const imageData = drawHistory.current[historyPointer.current]
+      context.putImageData(imageData, 0, 0)
+      dispatch(actionItemClick(null))
+    } 
+  }, [actionMenuItem])
 
   useEffect(() => {
     if (!canvasRef.current) return;
@@ -33,19 +64,22 @@ const Board = () => {
 
     const handleMouseDown = (e) => {
       shouldDraw.current = true
-      context.beginPath() 
+      context.beginPath()
       context.moveTo(e.clientX, e.clientY)
-      
+
     }
 
     const handleMouseMove = (e) => {
-      if(!shouldDraw.current) return
+      if (!shouldDraw.current) return
       context.lineTo(e.clientX, e.clientY)
       context.stroke()
     }
 
     const handleMouseUp = (e) => {
       shouldDraw.current = false
+      const imageData = context.getImageData(0, 0, canvas.width, canvas.height)
+      drawHistory.current.push(imageData)
+      historyPointer.current = drawHistory.current.length - 1
     }
 
     canvas.addEventListener('mousedown', handleMouseDown)
