079304ca1c9d14523346a62fc4331a3ebfe51567
mdtalha109 <mdtalha1041@gmail.com>
2023-11-12 17:27:22 +0530

    feat: added BE url based on node env

--- socket.js
+++ socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const URL = 'http://localhost:5000';
+const URL = process.env.NODE_ENV === 'production' ? 'https://sketch-board-backend-wa51.onrender.com' :  'http://localhost:5000';
 export const socket = io(URL);
