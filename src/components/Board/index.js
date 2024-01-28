315473406e8132c447873e4a2896636a3d221fd3
mdtalha109 <mdtalha1041@gmail.com>
2024-01-28 22:22:43 +0530

    feat: circle shape && socket event for square drawing

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,6 +1,6 @@
 import { actionItemClick } from '@/Redux/slice/menuSlice';
 import { MENU_ITEMS } from '@/constants';
-import React, { useEffect, useLayoutEffect, useRef } from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { socket } from '@/socket';
 import { drawRectangle } from '@/utilities/shapes/drawShape';
@@ -10,11 +10,9 @@ const Board = () => {
   const canvasRef = useRef(null)
   const shouldDraw = useRef(false)
   const drawHistory = useRef([])
-
-
   const historyPointer = useRef(0)
-
   const { activeMenuItem, actionMenuItem } = useSelector((state) => state.menu)
+  
   const { color, size } = useSelector((state) => state.toolbox[activeMenuItem])
 
   useEffect(() => {
@@ -34,8 +32,7 @@ const Board = () => {
       if (historyPointer.current > 0 && actionMenuItem === MENU_ITEMS.UNDO) historyPointer.current -= 1
       if (historyPointer.current < drawHistory.current.length - 1 && actionMenuItem === MENU_ITEMS.REDO) historyPointer.current += 1
       const imageData = drawHistory.current[historyPointer.current]
-      context.putImageData(imageData, 0, 0)
-
+      imageData && context.putImageData(imageData, 0, 0)
     }
     dispatch(actionItemClick(null))
   }, [actionMenuItem])
@@ -79,6 +76,7 @@ const Board = () => {
     if (!canvasRef.current) return;
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
+    context.scale(1, 1)
     
     if(activeMenuItem == 'PENCIL' || activeMenuItem == 'ERASER'){
      
@@ -120,13 +118,26 @@ const Board = () => {
       canvas.addEventListener('mousedown', handleMouseDown)
       canvas.addEventListener('mousemove', handleMouseMove)
       canvas.addEventListener('mouseup', handleMouseUp)
-  
+
+      const handleBeginPath = (path) => {
+        beginPath(path.x, path.y)
+      }
+
+      const handleDrawLine = (path) => {
+        drawLine(path.x, path.y)
+      }
+
+      socket.on('beginPath', handleBeginPath)
+      socket.on('drawLine', handleDrawLine)
   
   
       return () => {
         canvas.removeEventListener('mousedown', handleMouseDown)
         canvas.removeEventListener('mousemove', handleMouseMove)
         canvas.removeEventListener('mouseup', handleMouseUp)
+
+        socket.off('beginPath', handleBeginPath)
+        socket.off('drawLine', handleBeginPath)
   
     
       }
@@ -145,6 +156,7 @@ const Board = () => {
         shouldDraw.current = true
         coordinate.startCoordinate = { x: e.clientX, y: e.clientY };
         snapshot = context?.getImageData(0,0, canvas.width, canvas.height);
+        socket.emit('beginSquare')
       }
 
       const handleMouseMove = (e) => {
@@ -161,26 +173,41 @@ const Board = () => {
         context.strokeRect(
           Math.min(startX, endX),
           Math.min(startY, endY),
-          Math.abs(endX - startX), // width
-          Math.abs(endY - startY) // height
+          Math.abs(endX - startX), 
+          Math.abs(endY - startY) 
         );
+
+        socket.emit('drawSquare', coordinate)
       }
 
       const handleMouseUp = (e) => {
         shouldDraw.current = false
-        drawRectangle({
-          coordinate,
-          canvas,
-        });
-
         const imageData = context.getImageData(0, 0, canvas.width, canvas.height)
         drawHistory.current.push(imageData)
         historyPointer.current = drawHistory.current.length - 1
-        console.log("drawHistory: ", drawHistory)
       }
       canvas.addEventListener('mousedown', handleMouseDown)
       canvas.addEventListener('mousemove', handleMouseMove)
       canvas.addEventListener('mouseup', handleMouseUp)
+
+      const handleDrawSquare = (coordinate) => {
+        context.putImageData(snapshot, 0, 0)
+        const { x: startX, y: startY } = coordinate.startCoordinate;
+        const { x: endX, y: endY } = coordinate.endCoordinate;
+        context.strokeRect(
+          Math.min(startX, endX),
+          Math.min(startY, endY),
+          Math.abs(endX - startX), 
+          Math.abs(endY - startY) 
+        );
+      }
+
+      const handleBeginSquare = () => {
+        snapshot = context?.getImageData(0,0, canvas.width, canvas.height);
+      }
+
+      socket.on('beginSquare', handleBeginSquare)
+      socket.on('drawSquare', handleDrawSquare)
 
       return () => {
         canvas.removeEventListener('mousedown', handleMouseDown)
@@ -214,20 +241,16 @@ const Board = () => {
         coordinate.endCoordinate = { x: e.clientX, y: e.clientY };
         const { x: startX, y: startY } = coordinate.startCoordinate;
         const { x: endX, y: endY } = coordinate.endCoordinate;
-        
-        context.arc(50, 50, 50, 0, 2 * Math.PI)
+        context.beginPath();
+        const radius = Math.sqrt(
+          Math.pow(endX - startX, 2) + Math.pow(endY - startY, 2)
+        );
+        context.arc(startX, startY, radius, 0, 2 * Math.PI)
+        context.stroke() 
       }
 
       const handleMouseUp = (e) => {
         shouldDraw.current = false
-
-        drawRectangle({
-          coordinate,
-          canvas,
-        });
-
-        context.arc(50, 50, 50, 0, 2 * Math.PI);
-        context.stroke() 
 
         const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
         drawHistory.current.push(imageData);
@@ -245,11 +268,14 @@ const Board = () => {
       }
     }
 
-  
+   
+
   }, [activeMenuItem])
 
   return (
-    <canvas ref={canvasRef}></canvas>
+    <>
+      <canvas ref={canvasRef}></canvas>
+    </>
   )
 }
 
